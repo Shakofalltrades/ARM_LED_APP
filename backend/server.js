7cf066553f251f28bb5e2f3874770b7de6aced87
Sakhawat Salam <you@example.com>
2024-05-23 15:23:34 +0100

    Created the code to upload the animations to the database referred to in the workbench as "animations"

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const multer = require('multer');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+//file upload using multer
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage });
+
+//MYsql connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'armLED',
+    database: 'animations'
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL animations database');
+})
+
+app.post('/upload', upload.single('file'), (req,res) => {
+    const file = req.file;
+    if (!file) {
+        return res.status(400).send('No file uploaded.');
+    }
+
+    const sql = 'INSERT INTO images (name,data) VALUES (?, ?)';
+    const values = [file.originalname, file.buffer];
+
+    connection.query(sql, values, (err, result) => {
+        if (err) throw err;
+        res.send({message: 'File uploaded and saved to database', id: result.insertId});
+    });
+});
+
+app.listen(port, () => {
+    console.log('Server running on http://localhost:${port}');
+});
+
