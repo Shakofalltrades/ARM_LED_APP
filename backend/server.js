750f9933657e89b674942d628f54730385a3cc60
Sakhawat Salam <you@example.com>
2024-05-30 14:38:11 +0100

    Fixed the PNG transfer between the front end and the back end

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,49 @@
-const express = require('express');
-const multer = require('multer');
-const mysql = require('mysql');
-const cors = require('cors');
+import { fileURLToPath } from 'url';
+import path from 'path';
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import multer from 'multer';
+import fs from 'fs';
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
 
+// Convert import.meta.url to __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Ensure the upload directory exists
+const uploadDir = path.join(__dirname, 'upload');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 //file upload using multer
-const storage = multer.memoryStorage();
-const upload = multer({storage: storage });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'upload/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  const upload = multer({ storage: storage });
 
 //MYsql connection
-const connection = mysql.createConnection({
+const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'armled',
     database: 'animations'
 })
 
-connection.connect((err) => {
+con.connect((err) => {
     if (err) throw err;
     console.log('Connected to MySQL animations database');
+
 })
 
 app.post('/upload', upload.single('file'), (req,res) => {
@@ -31,7 +52,7 @@ app.post('/upload', upload.single('file'), (req,res) => {
         return res.status(400).send('No file uploaded.');
     }
 
-    const sql = 'INSERT INTO images (name,data) VALUES (?, ?)';
+    const sql = 'INSERT INTO test (id,frames) VALUES (?, ?)';
     const values = [file.originalname, file.buffer];
 
     connection.query(sql, values, (err, result) => {
